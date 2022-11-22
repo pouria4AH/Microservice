2a0c72297dd5d50888ecf4dcbe82cc1999464ff6
net developer <4pouria.ahmadi.2oo1@gmail.com>
2022-11-23 00:13:48 +0330

    add redise config in basket project

---
+++ Services/Basket/Basket.api/Entities/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.api.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            userName = UserName;
+        }
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (ShoppingCartItem item in ShoppingCartItems)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
--- Services/Basket/Basket.api/Startup.cs
+++ Services/Basket/Basket.api/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basket.api
 {
@@ -27,6 +21,10 @@ namespace Basket.api
         {
 
             services.AddControllers();
+            services.AddStackExchangeRedisCache(optiones =>
+            {
+                optiones.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.api", Version = "v1" });
