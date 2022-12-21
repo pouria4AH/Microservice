5959d484f93495fa1d74c92a8c21b73d5602abf9
net developer <4pouria.ahmadi.2oo1@gmail.com>
2022-12-21 16:59:52 +0330

    debug project

--- Services/Catalog/catalog.Api/Controllers/CatalogController.cs
+++ Services/Catalog/catalog.Api/Controllers/CatalogController.cs
@@ -40,9 +40,9 @@ namespace catalog.Api.Controllers
         [HttpGet("{id:length(24)}", Name = "GetProduct")]
         [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProduct(string id)
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct([FromRoute]string id)
         {
-            var product = await _productRepository.GetProductByName(id);
+            var product = await _productRepository.GetProductById(id);
             if (product == null)
             {
                 _logger.LogError($"product with id :{id} not found");
@@ -55,7 +55,7 @@ namespace catalog.Api.Controllers
         #region get products by categories 
         [HttpGet("[action]/{category}")]
         [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory([FromRoute]string category)
         {
             var product = await _productRepository.GetProductByCategory(category);
             return Ok(product);
@@ -76,7 +76,7 @@ namespace catalog.Api.Controllers
         #region update product
         [HttpDelete("{id:length(24)}",Name = "DeleteProduct")]
         [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<Product>> UpdateProduct(string id)
+        public async Task<ActionResult<Product>> DeleteProduct(string id)
         {
             return Ok(await _productRepository.DeleteProduct(id));
         }
