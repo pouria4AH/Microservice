ecc2e6ce2a214d8dc888f6d34f4c377a24a155ed
net developer <4pouria.ahmadi.2oo1@gmail.com>
2023-01-28 00:49:09 +0330

    debug project and complite crud opertion in discount grpc

--- Services/Discount/DiscountGrpc/Services/DiscountService.cs
+++ Services/Discount/DiscountGrpc/Services/DiscountService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using AutoMapper;
+using Discount.Grpc.Entities;
 using Discount.Grpc.Protos;
 using Discount.Grpc.Repositories;
 using Grpc.Core;
@@ -38,6 +39,43 @@ namespace Discount.Grpc.Services
             _logger.LogInformation("discount is Retrived by product name");
             return _Mapper.Map<CouponModel>(coupon);
 
+        }
+
+        #endregion
+
+        #region create discount
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscounrRequset request, ServerCallContext context)
+        {
+            var coupon = _Mapper.Map<Coupon>(request.Coupn);
+            await _discountRepository.CrateDiscount(coupon);
+            _logger.LogInformation($"Discount create successfully for {coupon.ProductName}");
+            return _Mapper.Map<CouponModel>(coupon);
+        }
+
+        #endregion
+
+        #region update discount
+
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscounrReqset request, ServerCallContext context)
+        {
+            var coupon = _Mapper.Map<Coupon>(request.Coupn);
+            await _discountRepository.UpdateDiscount(coupon);
+            _logger.LogInformation($"discount update successfully for {coupon.ProductName} ");
+            return _Mapper.Map<CouponModel>(coupon);
+        }
+
+        #endregion
+
+        #region delete discount
+
+        public override async Task<DeleteDiscounrResponse> DeleteDiscount(DeleteDiscounrRequset request, ServerCallContext context)
+        {
+            var deleted = await _discountRepository.DeleteDiscount(request.ProductName);
+            return new DeleteDiscounrResponse
+            {
+                Succes = deleted
+            };
         }
 
         #endregion
